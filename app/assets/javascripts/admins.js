4c90e0bc01482a76f2ae8b36e7e90b8188baf88b
Mirko Cerovac <mirko.cerovac@gmail.com>
2016-02-26 22:11:55 +0100

    Changes in admin users panel.

--- app/assets/javascripts/admins.js
+++ app/assets/javascripts/admins.js
@@ -1,3 +1,15 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+$( document ).ready(function() {
+
+  $('.users-collapse').on('click', function(ev){
+    var $icon = $(this);
+    var $collapse_target = $('.users-panel-body');
+    if ($collapse_target.is(':visible')) {
+      $collapse_target.hide('');
+      $icon.removeClass('fa-minus-square-o').addClass('fa-expand');
+    } else {
+      $icon.removeClass('fa-expand').addClass('fa-minus-square-o');
+      $collapse_target.show('');
+    }
+  });
+
+});
